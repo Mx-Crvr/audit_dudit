d1a48f8382228a8a35a51107437d3ca34a856d8d
Mx-Crvr <mcevans.dev@gmail.com>
2023-08-18 11:46:41 +0200

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/*./{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      black: '#16161D',
+      white: '#F0F0F0',
+      blue: '#1E90FF',
+      darkblue: '#1E20FF',
+    }
   },
   plugins: [],
 }
