4d82ecdced3cf1e2f24d8677511f545d829d0efd
Mx-Crvr <mcevans.dev@gmail.com>
2023-08-19 21:12:05 +0200

    corrected some errors

--- dist/js/audit.js
+++ dist/js/audit.js
@@ -2,4 +2,56 @@
 
 document.getElementById('back-btn').addEventListener('click', () => {
   window.history.back()
-})
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const validationMessage = document.getElementById('validationMessage');
+  const proceedBtn = document.getElementById('proceedBtn');
+  const questions = document.querySelectorAll('.question');
+
+  proceedBtn.addEventListener('click', () => {
+    let allQuestionsAnswered = true;
+
+    questions.forEach((question) => {
+      const questionDiv = question.parentElement;
+      const inputs = questionDiv.querySelectorAll('input[type="radio"]');
+      
+      let anyAnswered = false;
+      
+      inputs.forEach((input) => {
+        if (input.checked) {
+          anyAnswered = true
+        }
+      });
+      
+      if (!anyAnswered) {
+        allQuestionsAnswered = false;
+        questionDiv.classList.add('unanswered');
+      } else {
+        questionDiv.classList.remove('unanswered');
+      }
+    });
+      
+    if (!allQuestionsAnswered) {
+      validationMessage.classList.remove('hidden');
+    } else {
+      validationMessage.classList.add('hidden');
+      caluclateScore()
+    }
+  });
+});
+
+function caluclateScore() {
+  let a_score = 0;
+
+  const inputs = document.querySelectorAll('input[type="radio"]')
+  inputs.forEach((input) => {
+    if (input.checked) {
+      a_score += parseInt(input.value)
+    }
+  })
+
+  localStorage.setItem('a_score', a_score);
+
+  window.location.href = './results.html'
+}
---
+++ dist/js/dudit.js
@@ -0,0 +1,59 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', () => {
+  const validationMessage = document.getElementById('validationMessage');
+  const proceedBtn = document.getElementById('proceedBtn');
+  const questions = document.querySelectorAll('.question');
+
+  proceedBtn.addEventListener('click', () => {
+    let allQuestionsAnswered = true;
+
+    questions.forEach((question) => {
+      const questionDiv = question.parentElement;
+      const inputs = questionDiv.querySelectorAll('input[type="radio"]');
+      
+      let anyAnswered = false;
+      
+      inputs.forEach((input) => {
+        if (input.checked) {
+          anyAnswered = true
+        }
+      });
+      
+      if (!anyAnswered) {
+        allQuestionsAnswered = false;
+        questionDiv.classList.add('unanswered');
+      } else {
+        questionDiv.classList.remove('unanswered');
+      }
+    });
+      
+    if (!allQuestionsAnswered) {
+      validationMessage.classList.remove('hidden');
+    } else {
+      validationMessage.classList.add('hidden');
+      caluclateScore()
+    }
+  });
+});
+
+function caluclateScore() {
+  let d_score = 0;
+
+  const inputs = document.querySelectorAll('input[type="radio"]')
+  inputs.forEach((input) => {
+    if (input.checked) {
+      d_score += parseInt(input.value)
+    }
+  })
+
+
+
+  console.log(d_score)
+
+  localStorage.setItem('d_score', d_score);
+
+  window.location.href = './a_test.html'
+}
+
+
